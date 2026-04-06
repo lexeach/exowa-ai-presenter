7f7ba7abebae45bc753b140b685638d4bbd1231c
lexeach <80155340+lexeach@users.noreply.github.com>
2026-04-06 16:29:04 +0530

    Fix import path for ObservabilityDashboard

--- src/pages/LiveReportPage.jsx
+++ src/pages/LiveReportPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ObservabilityDashboard from "../pages/ObservabilityDashboard";
+import ObservabilityDashboard from "./ObservabilityDashboard";
 
 function LiveReportPage() {
   return (
